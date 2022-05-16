9c0d1f0067723bb5c29a37357f0e2d4600082e8b
wisniax <101927988+wisniax@users.noreply.github.com>
2022-05-16 21:09:53 +0200

    fix

--- TetrisSFML/arena.h
+++ TetrisSFML/arena.h
@@ -1,20 +1,2 @@
 #pragma once
 
-class box
-{
-private:
-	RectangleShape rectangle
-		setColor()
-	{
-
-	}
-public:
-	box()
-	{
-		// define a 120x50 rectangle
-		RectangleShape rectangle(Vector2f(120, 50));
-
-		// change the size to 100x100
-		rectangle.setSize(Vector2f(100, 100));
-	}
-};
