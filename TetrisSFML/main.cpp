60e4c4e8f07e004c8a856127b5a07c47608bb6f2
Bazyliii <jaroslawierzbowski2001@gmail.com>
2022-05-16 21:25:34 +0200

    chuj kurwa

--- TetrisSFML/main.cpp
+++ TetrisSFML/main.cpp
@@ -10,33 +10,5 @@ using namespace std;
 
 
 int main(void) {
-	RenderWindow window(VideoMode(1920, 1080), "TETRIS", Style::Fullscreen);
-	while (window.isOpen())
-	{
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed) {
-				window.close();
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Escape))
-			{
-				window.close();
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Left))
-			{
-				cout << "Lewo";
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Right))
-			{
-				cout << "Prawo";
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Down))
-			{
-				cout << "Dol";
-			}
-		}
-		window.clear(Color::Black);
-		window.display();
-	}
+	
 }
