e2942234df5180b75161f49d4a0bf28e5e0b0808
Bazyliii <jaroslawierzbowski2001@gmail.com>
2022-05-17 18:19:43 +0200

    chuj^2

--- TetrisSFML/Window.h
+++ TetrisSFML/Window.h
@@ -23,17 +23,17 @@ public:
 		window.setFramerateLimit(60);
 		while (window.isOpen())
 		{
-			RectangleShape chujnia[(arenaWidth+2)*(arenaHeight+2)];
-			int chujnia_length = 0;
+			RectangleShape renderList[(arenaWidth+2)*(arenaHeight+2)];
+			int list_length = 0;
 			for (int j = 0; j < arenaHeight + 2;j++) {
 				for (int i = 0;i < arenaWidth + 2;i++) {
 					if (usedColors::backgroundColor==arena.tempMatrix[j][i]) {
 						continue;
 					}
-					RectangleShape chuj(Vector2f(SIZE, SIZE));
-					chuj.setFillColor(arena.tempMatrix[j][i]);
-					chuj.setPosition(Vector2f(i * SIZE, j * SIZE));
-					chujnia[chujnia_length++] = chuj;
+					RectangleShape x(Vector2f(SIZE, SIZE));
+					x.setFillColor(arena.tempMatrix[j][i]);
+					x.setPosition(Vector2f(i * SIZE, j * SIZE));
+					renderList[list_length++] = x;
 				}
 			}
 
@@ -46,8 +46,8 @@ public:
 			window.setActive();
 			window.clear();
 			//rysowanie
-			for (int i = 0;i <chujnia_length;i++) {
-				window.draw(chujnia[i]);
+			for (int i = 0;i <list_length;i++) {
+				window.draw(renderList[i]);
 			}
 
 			window.display();
