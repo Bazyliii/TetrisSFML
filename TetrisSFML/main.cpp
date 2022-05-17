de840a7c467f2c76707ed81c1c474d70900cbeaf
Bazyliii <jaroslawierzbowski2001@gmail.com>
2022-05-17 14:28:05 +0200

    a

--- TetrisSFML/main.cpp
+++ TetrisSFML/main.cpp
@@ -1,7 +1,5 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Main.hpp>
-#include <chrono>
-#include "Game.h"
 
 using namespace sf;
 using namespace std;
@@ -9,31 +7,17 @@ using namespace std;
 
 
 int main(void) {
-	RenderWindow window(VideoMode(800, 900), "Tetris");
-
-	auto tp{ chrono::system_clock::now() };
-	static constexpr float FRAME_RATE{ 60.0f };
-	static GameState* state = &GameState::menu;
-
-	Game game(state);
-
-	while (window.isOpen()) {
-		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed) {
-				window.close();
-			}
-		}
-		float dt;
-		{
-			auto new_tp{ chrono::system_clock::now() };
-			dt = chrono::duration<float>(new_tp - tp).count();
-			tp = new_tp;
-		}
-		game.handleInput();
-		game.update(dt * FRAME_RATE);
-		window.clear();
-		window.draw(game);
-		window.display();
-	}
+    Window window(VideoMode(800, 600), "Tetris");
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        window.setActive();
+        window.display();
+    }
 }
