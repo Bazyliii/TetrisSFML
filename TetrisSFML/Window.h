157f726e9a78145275bf153c6bff7e89edba4314
Bazyliii <jaroslawierzbowski2001@gmail.com>
2022-05-17 17:52:25 +0200

    chuj kurwa

--- TetrisSFML/Window.h
+++ TetrisSFML/Window.h
@@ -3,33 +3,53 @@
 #include <SFML/Main.hpp>
 #include "TetrisArena.h"
 #include "Tetrino.h"
+#define SIZE 40
+
 using namespace sf;
 
-class AppWindow 
+class AppWindow
 {
 private:
-    Arena arena;
-    Window window;
+	Arena arena;
+	RenderWindow window;
+
 public:
 	AppWindow() :
-        window(VideoMode(800, 600), "Tetris")
-    {
-        arena = Arena();
-        arena.printBlock(Box());
+		window(VideoMode(800, 600), "Tetris")
+	{
+		arena = Arena();
+		arena.printBlock(Box());
+		window.setFramerateLimit(60);
+		while (window.isOpen())
+		{
+			RectangleShape chujnia[arenaWidth+2];
+			int chujnia_length = 0;
+			for (int j = 0; j < arenaHeight + 2;j++) {
+				for (int i = 0;i < arenaWidth + 2;i++) {
+					if () {
+					
+					}
+					RectangleShape chuj(Vector2f(SIZE, SIZE));
 
-        window.setFramerateLimit(60);
-        while (window.isOpen())
-        {
+					chuj.setPosition(Vector2f(i * SIZE, j * SIZE));
+					chujnia[chujnia_length++] = chuj;
+				}
+			}
 
-            Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == Event::Closed)
-                    window.close();
-            }
-            window.setActive();
-            window.display();
+			Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == Event::Closed)
+					window.close();
+			}
+			window.setActive();
+			window.clear();
+			//rysowanie
+			for (int i = 0;i <chujnia_length;i++) {
+				window.draw(chujnia[i]);
+			}
 
-        }
+			window.display();
+		}
 	}
 };
