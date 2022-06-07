05f06b08e9067340dc3b8c56b079cbf012084a03
wisniax <101927988+wisniax@users.noreply.github.com>
2022-06-08 01:42:41 +0200

    SCOREEE LEADERBOARD INCOOOMING

--- TetrisSFML/Score.h
+++ TetrisSFML/Score.h
@@ -10,6 +10,10 @@ class Score
 private:
 	static int points;
 	static sf::Font font;
+	static bool sortbysec(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b)
+	{
+		return (b.second < a.second);
+	}
 public:
 	static void init();
 	static sf::Text getScoreAsText();
@@ -22,16 +26,59 @@ public:
 		FileIO::appendToFile(name + ":" + std::to_string(getScore()), "score");
 	}
 	//Returns top 10 scores
-	static std::string getTopScore() 
+	static std::string getTopScore()
 	{
 		std::string score = FileIO::readFromFile("score");
-		int numberOfEntrys = 0;
-		for (auto& ch : score)
+
+		//convert given string to vector of pairs of name;score
+		std::vector<std::pair<std::string, int>> scorePairs;
 		{
-			if (ch == '\n') numberOfEntrys++;
+			bool isValue = false;
+			std::string strPair = "";
+			std::string intPair = "";
+			std::pair<std::string, int> tempPair = std::pair<std::string, int>();
+			for (auto& ch : score)
+			{
+				switch (ch)
+				{
+				case '\n':
+					tempPair = std::pair<std::string, int>();
+					tempPair.first = strPair;
+					tempPair.second = stoi(intPair);
+					scorePairs.push_back(tempPair);
+					strPair = "";
+					isValue = false;
+					break;
+				case ':':
+					intPair = "0";
+					isValue = true;
+					break;
+				default:
+					switch (isValue)
+					{
+					case true:
+						intPair += ch;
+						break;
+					case false:
+						strPair += ch;
+						break;
+					}
+					break;
+				}
+			}
 		}
 
+		std::sort(scorePairs.begin(), scorePairs.end(), sortbysec);
 
-		return "";
+		std::string str = "";
+		for (int i = 0; i < 10 && i < scorePairs.size(); i++)
+		{
+			str.append(scorePairs[i].first);
+			str.append(" : ");
+			str.append(std::to_string(scorePairs[i].second));
+			str += '\n';
+		}
+
+		return str;
 	}
 };
