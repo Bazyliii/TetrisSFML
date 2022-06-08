f1b899504c8dd98a194a0e5b0893aaa93a827b6e
wisniax <101927988+wisniax@users.noreply.github.com>
2022-06-08 13:36:19 +0200

    No more win api

--- TetrisSFML/main.cpp
+++ TetrisSFML/main.cpp
@@ -1,13 +1,10 @@
 #include "Window.h"
 #include "Sounds.h"
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 
 using namespace std;
 
 int main(void) 
 {
-	ShowWindow(GetConsoleWindow(), SW_HIDE);
 	Sounds::init();
 	Sounds::playmusic();
 	AppWindow appWindow;
